25970ea90082a24998b6e496a016dadc58a78f4f
mariiahorbova <gorbova21@gmail.com>
2022-02-08 03:12:51 +0200

    new pet project

---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """Topic that user learns"""
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return string-formatted model"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Some distinct information about the theme"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return string-formatted model"""
+        # Cuts string with '...' if string length is more than 50 characters
+        return f"{self.text[:50]}..." if len(self.text) > 50 else self.text
