cb007a5d03f32860c4a9b4f7287b9eea048fa3ce
mariiahorbova <gorbova21@gmail.com>
2022-02-09 02:02:34 +0200

    django works ^_^

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'Learning_logs'
+app_name = "Learning_logs"
 urlpatterns = [
     # Main page
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    # Page of all topics
+    path("topics/", views.topics, name="topics"),
+    # Page of a specific topic
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
 ]
