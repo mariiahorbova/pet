e698f8a820ea80f71aade969fd4c80f977cc5b87
mariiahorbova <gorbova21@gmail.com>
2022-02-09 03:18:43 +0200

    added topics and entries

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -21,3 +22,40 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """ Add new topic """
+    if request.method != 'POST':
+        # No data sent; create an empty form
+        form = TopicForm()
+    else:
+        # POST sent; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Show empty or not valid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """ Add a new entry for a particular topic """
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data sent; create an empty form
+        form = EntryForm()
+    else:
+        # POST sent; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Show empty or not valid form
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
